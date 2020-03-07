1ef50ac549c50da480ee92d9eed3186696163b07
pat.riots <maryna567@gmail.com>
2020-03-07 16:07:57 +0100

    add files

---
+++ first.py
@@ -0,0 +1,11 @@
+print("ola")
+if 3>2:
+    print("ok")
+else:
+    print('no')
+A=3
+B=4
+C=5
+if (A+B)>C and (A+C)>B and (B+C)>A:
+    print("triangle")  
+
---
+++ python_intro.py
@@ -0,0 +1,20 @@
+def hallo1(name):
+    if name=="ola":
+       print("hallo")
+    elif name=="sonja":   
+       print("hallo sonja")
+    else:
+       print("dont know you")
+hallo1("liza")
+girls=["monica","rachel","phoebe","ola","you"]
+def hallo(name):
+        print("hallo"+name+"!")
+for name in girls:
+    hallo(name)
+        print("next girl")
+hallo("liz")
+
+
+
+
+
